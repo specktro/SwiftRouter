6659b5fa3c76625cfb5809f0e2c38b2d640ed4e7
specktro <specktro@appetito.mx>
2017-10-10 12:15:56 -0500

    chore: Initial commit with gitignore file from https://www.gitignore.io/

---
+++ Router/Router.h
@@ -0,0 +1,19 @@
+//
+//  Router.h
+//  Router
+//
+//  Created by specktro on 10/10/17.
+//  Copyright © 2017 specktro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Router.
+FOUNDATION_EXPORT double RouterVersionNumber;
+
+//! Project version string for Router.
+FOUNDATION_EXPORT const unsigned char RouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Router/PublicHeader.h>
+
+
